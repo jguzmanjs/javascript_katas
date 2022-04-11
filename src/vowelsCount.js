6934782c6bbcc08bb3eb58457d253f8605c02ca0
Miguel <gallegorodriguezmiguel58@gmail.com>
2022-04-11 10:46:33 +0200

    solucion ejercicio

--- src/vowelsCount.js
+++ src/vowelsCount.js
@@ -2,10 +2,20 @@
 
 function getCount(str) {
     var vowelsCount = 0;
-
+    
     // enter your majic here
+    let letra = str.split('');
+   
+    for(let i =0; i<letra.length; i++){
+        if(letra[i] == 'a' || letra[i] == 'e' || letra[i] == 'i' || letra[i] == 'o' || letra[i] == 'u'){
 
+                vowelsCount++;
+
+        }
+
+    }
     return vowelsCount;
 }
+
 
 module.exports = getCount;
